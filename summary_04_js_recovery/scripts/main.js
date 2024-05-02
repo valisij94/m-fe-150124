b1cdae7365cc261f18d1e55149bdd9d0e3eb664e
Davy <davyd.dabahian@cheetahdigital.com>
2024-05-03 00:01:56 +0300

    summary_04 recovery

---
+++ summary_04_js_recovery/scripts/main.js
@@ -0,0 +1,12 @@
+const address = {
+  name: 'Persistent address',
+  city: 'City',
+  street: 'Street',
+  house: 10
+}
+
+const products = [
+  { name: 'Ball', countryMade: 'China', price: 10},
+  { name: 'Gloves', countryMade: 'Georgia', price: 15},
+  { name: 'Shoes', country: 'Armenia', price: 20}
+]
