d75bf05f1ab693d093268f058a8f85c54b09fde8
Davy <davyd.dabahian@cheetahdigital.com>
2024-05-16 00:04:08 +0300

    practice 13 array methods advanced

--- practice_12_array_methods_callback/scripts/main.js
+++ practice_12_array_methods_callback/scripts/main.js
@@ -3,8 +3,26 @@
 // Массив чисел (примитивы)
 const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11,9,2,7];
 
-// Массив строк (примитивы)
-const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+/*
+1. Пристрелочная. Пройтись по массиву чисел, для каждого числа выводить в консоль, является ли оно четным.
+*/
+/*
+numbers.forEach( (elem) => elem % 2 === 0 ? console.log(`${elem} is even`) : console.log(`${elem} is odd`) );
+
+numbers.forEach( elem => {
+    if (elem % 2 === 0) {
+        console.log('Even');
+    }
+    else {
+        console.log('Odd');
+    }
+});
+*/
+
+/*
+2. Пройтись по массиву строк, для каждой строки, если она меньше 4 символов, выводить в консоль `short`, иначе - `long`
+*/
+// strings.forEach( (elem) => console.log(elem.length < 4 ? `${elem} short` : `${elem} long`));
 
 const ball = {
     name: "Ball",
@@ -47,3 +65,50 @@ const shark = {
 
 // Массив объектов
 let products = [ball, gloves, shoes, hammer, saw];
+
+/*
+3. Пройтись по массиву товаров, вывести в консоль названия всех товаров
+*/
+// products.forEach( product => console.log(product.name) );
+
+/*
+4. Из массива чисел, получить новый массив, который будет содержать квадраты чисел
+*/
+// const squares = numbers.map( num => num ** 2 );
+// console.log(squares)
+
+/*
+5. Из массива строк получить новый массив, который будет содержать массив ОБЪЕКТОВ, у которых будут свойства:
+     * value - сама строка
+     * size - если длина строки меньше 4 символов, то `short`, иначе - `long`
+*/
+// Массив строк (примитивы)
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+// const objects = strings.map( elem => ({
+//     value: elem,
+//     size: elem.length >= 4 ? 'long' : 'short'
+// }));
+// console.log(objects);
+
+// 6. Из массива товаров получить массив имен товаров
+// let productNames = products.map( prod => prod.name);
+// console.log(productNames);
+
+// // REDUCE
+// let productNamesFromReduce = products.reduce( ( acc, val) => {
+//     acc.push(val.name);
+//     return acc;
+// }, [] );
+// console.log(productNamesFromReduce);
+
+/*
+7. Из массива товаров получить массив "уцененных" товаров. В новом массиве должны быть объекты товаров, у которых цена вдвое меньше, чем у исходных товаров.
+*/
+
+const discountedProducts = products.map( (prod) => ({
+    ...prod,
+    price: prod.price / 2,
+}));
+
+console.log(discountedProducts);
