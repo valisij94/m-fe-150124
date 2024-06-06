f03a850ad2be338b2a6ca3b8b66da09981c5ed1d
Davy <davyd.dabahian@cheetahdigital.com>
2024-06-06 23:24:18 +0300

    summary 09 async

--- summary_09_async_operations/scripts/main.js
+++ summary_09_async_operations/scripts/main.js
@@ -7,24 +7,3 @@ const racers = [
 ];
 
 const winners = [];
-
-function startRace(racer) {
-  const rand = Math.random();
-  return new Promise( (resolve, reject) => {
-    setTimeout( () => {
-      resolve(racer);
-    }, rand * 1000);
-  })
-};
-
-function finishRace(racer) {
-  if (winners.length < 3) {
-    winners.push(racer);
-    console.log('Racer ' + racer.name + ' finished!');
-  }
-  else {
-    console.log('Racer ' + racer.name + ' was too slow...')
-  }
-}
-
-racers.forEach( racer => startRace(racer).then(finishRace));
