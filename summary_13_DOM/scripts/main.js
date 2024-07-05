9518548d2077cd5604a62cbf249422a406782e16
Davy <davyd.dabahian@cheetahdigital.com>
2024-07-05 10:30:41 +0300

    summary 13

--- summary_13_DOM/scripts/main.js
+++ summary_13_DOM/scripts/main.js
@@ -22,7 +22,6 @@ const drive = () => {
     robotTop++;
     robot.style.top = `${robotTop * robotHeight}px`;
   }
-  checkCross();
 }
 
 const reverse = () => {
@@ -30,7 +29,6 @@ const reverse = () => {
     robotTop--;
     robot.style.top = `${robotTop * robotHeight}px`;
   }
-  checkCross();
 }
 
 const left = () => {
@@ -38,7 +36,6 @@ const left = () => {
     robotLeft++;
     robot.style.left = `${robotLeft * robotWidth}px`;
   }
-  checkCross();
 }
 
 const right = () => {
@@ -46,7 +43,6 @@ const right = () => {
     robotLeft--;
     robot.style.left = `${robotLeft * robotWidth}px`;
   }
-  checkCross();
 }
 
 const btnDrive = document.querySelector('.drive');
@@ -58,37 +54,3 @@ btnDrive.addEventListener('click', drive);
 btnReverse.addEventListener('click', reverse);
 btnLeft.addEventListener('click', left);
 btnRight.addEventListener('click', right);
-
-
-const addApples = (applesCount) => {
-  const result = [];
-  while (result.length < applesCount) {
-    let rand1 = Math.min(Math.round(Math.random() * 10), 9);
-    let rand2 = Math.min(Math.round(Math.random() * 10), 9);
-    const exists = result.some( el => el.x === rand1 && el.y === rand2 );
-    if (!exists) {
-      result.push( { x: rand1, y: rand2 } );
-    }
-  }
-  for (let apple of result) {
-    const appleEl = document.createElement('div');
-    appleEl.classList.add('apple');
-    appleEl.style.top = `${apple.y * 50}px`;
-    appleEl.style.left = `${apple.x * 50}px`;
-    apple.element = appleEl;
-    field.append(appleEl);
-  }
-  return result;
-}
-
-
-let apples = addApples(10);
-
-const checkCross = () => {
-  const apple = apples.find( el => el.x === robotLeft && el.y === robotTop);
-  if (apple) {
-    console.log(apple)
-    apple.element.style.opacity = 0;
-    apples = apples.filter( el => el !== apple);
-  }
-}
