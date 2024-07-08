9436af3459aada2286a4944870682d32d534f029
Davy <davyd.dabahian@cheetahdigital.com>
2024-07-08 23:59:52 +0300

    pracitce 28 bubbling

---
+++ practice_28_DOM_events/scripts/main.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modalBackground');
+
+function showModal() {
+  modal.style.display = 'block';
+}
+
+function hideModal() {
+  modal.style.display = 'none';
+}
