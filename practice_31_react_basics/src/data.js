434e74f5722b665fd49357b3ecef283e88894eb7
Davy <davyd.dabahian@cheetahdigital.com>
2024-07-29 23:17:31 +0300

    practice 31 react basics

---
+++ practice_31_react_basics/src/data.js
@@ -0,0 +1,16 @@
+export const simpleList = ['Gloves', 'Shoes', 'T-Shirt', 'Jeans'];
+
+export const complicatedList = [
+  {
+    name: 'Hammer',
+    price: 100
+  },
+  {
+    name: 'Drill',
+    price: 200
+  },
+  {
+    name: 'Saw',
+    price: 150
+  }
+]
