49b2acedc8562afad8c26b029b5ff99c2e088a3f
Davy <davyd.dabahian@cheetahdigital.com>
2024-08-20 21:55:41 +0300

    practice 38 useEffect

---
+++ practice_38_react_useEffect/src/components/LoginForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Copyright from "./Copyright";
+
+function LoginForm( {setUserLogin} ) {
+
+  const [login, setLogin] = useState('');
+  const [passwd, setPasswd] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value);
+  }
+
+  const handlePasswdChange = (event) => {
+    setPasswd(event.target.value);
+  }
+
+  const handleLoginClick = () => {
+    if (!!login && !!passwd) {
+      console.log(login, passwd);
+      setError(false);
+      setUserLogin(login);
+    }
+    else {
+      setError(true);
+    }
+  }
+
+  return (
+    <div>
+      <input value={login} onChange={handleLoginChange} type="text" placeholder="Login"/>
+      <input value={passwd} onChange={handlePasswdChange} type="password" placeholder="Password"/>
+      { error && <p>Fill all fields!</p> }
+      <button onClick={handleLoginClick}>Login</button>
+    </div>
+  )
+}
+
+export default LoginForm;
